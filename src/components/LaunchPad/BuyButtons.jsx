baecfffddf381568f58bd71aebf2b463964f2787
Codexjay <jaydencolins08@gmail.com>
2023-02-10 15:28:50 +0100

    update to styling

--- src/components/LaunchPad/BuyButtons.jsx
+++ src/components/LaunchPad/BuyButtons.jsx
@@ -1,10 +1,14 @@
+import border from '../assets/icons/buy-btn-border.svg';
+
 export default function BuyButtons() {
   return (
     <section className="buy-btns">
       <div className="">
+        <img src={border} alt="border" />
         <a href="" className="buy-btn">Buy on Equaliser</a>
       </div>
       <div className="">
+      <img src={border} alt="border" />
         <a href="" className="buy-btn">Buy on Firebird</a>
       </div>
     </section>
